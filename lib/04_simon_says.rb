d601785f071476a370621a7bdba466a012b99045
BrvAlexis <brevieralexis@gmail.com>
2024-01-22 15:10:21 +0100

    finish

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -6,6 +6,11 @@ def shout(message)
     message.upcase
 end
 
+def repeat(message, times = 2)
+    result = [message] * times
+    result.join(' ')
+  end
+
 def start_of_word(word, n)
     word[0, n]
 end
@@ -14,3 +19,10 @@ def first_word(sentence)
     sentence.split.first # méthode split pour diviser la phrase en mots et la méthode first pour obtenir le premier mot
 end
 
+def titleize(title)
+    little_words = %w(and the over)
+    words = title.split.map.with_index do |word, index|
+      (index.zero? || !little_words.include?(word.downcase)) ? word.capitalize : word
+    end
+    words.join(' ')
+end
